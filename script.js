7ad2dbe64759b105121c4ec1e88a3b98d1892f98
Wiand1515 <marofual@gmail.com>
2021-08-10 03:52:12 -0400

    atribute filters add

--- script.js
+++ script.js
@@ -3,13 +3,12 @@ const $template = document.getElementById('template-producto').content,
 $contentBody = document.getElementById('content-body'),
 $fragment = document.createDocumentFragment();
 
+//Obtiene todos los productos
 const getProducts = async (url) => {
    let res = await fetch(url),
     json = await res.json();  
         
-        json.data.forEach(el => {
-            console.log(el);
-    
+        json.data.forEach(el => {    
             $template.querySelector("img").setAttribute("src", el.url_image);
             $template.querySelector('h5').textContent = el.name;
             $template.querySelector('span').textContent = el.price
@@ -22,6 +21,22 @@ const getProducts = async (url) => {
 
 };
 
+//Obtiene los productos por categoria
+const getProductsCategory = async (url) => {
+    let res = await fetch(url),
+    json = await res.json();
+
+    json.data[0].products.forEach(el => {    
+        $template.querySelector("img").setAttribute("src", el.url_image);
+        $template.querySelector('h5').textContent = el.name;
+        $template.querySelector('span').textContent = el.price
+
+        let $clone = document.importNode($template, true);
+        $fragment.appendChild($clone);
+    });
+    $contentBody.appendChild($fragment);
+}
+
 
 //Cargar todos los productos a la carga del DOM
 document.addEventListener('DOMContentLoaded', () => {
@@ -29,16 +44,23 @@ document.addEventListener('DOMContentLoaded', () => {
     getProducts("http://localhost:8080/api/productos/");
 });
 
-//Delegacion de eventos on click => para agregar a carrito 
+//Delegacion de eventos on click => para agregar a carrito y filtrar por categorias
 document.addEventListener('click', (e) => {
+    
+    //Agrega al carrito
     if(e.target.matches('.boton-agregar')) {
         alert('Boton agregar')
     };
     
+    //Filtra por categoria
     if(e.target.matches('.botones-hilera button')) {
-        $contentBody.innerHTML = '';
-        alert(e.target.value);
+       $contentBody.innerHTML = '';
+
+       if(e.target.value == 0) {
         getProducts("http://localhost:8080/api/productos/");
+       } else {
+        getProductsCategory(`http://localhost:8080/api/search/categorias/${e.target.value}`);
+       }
     }
 });
 
